f869fc8d3119096f32bcb4f0926b913b47553dc8
David <david.hov.1993@gmail.com>
2025-12-26 18:51:54 +0100

    Initial Commit

---
+++ AI coach WORKOUT/AI_coach_WORKOUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_coach_WORKOUTApp.swift
+//  AI coach WORKOUT
+//
+//  Created by Давид on 26.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct AI_coach_WORKOUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
